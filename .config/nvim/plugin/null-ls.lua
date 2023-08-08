5e2e9d0721b13519ad85c89f0e5537ce14cb08f8
Ivo <ivo@dunamu.com>
2023-08-08 10:34:49 +0900

    refactor: replace golang formatter

--- .config/nvim/plugin/null-ls.lua
+++ .config/nvim/plugin/null-ls.lua
@@ -19,7 +19,7 @@ null_ls.setup({
     --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
     formatting.prettier, -- js/ts formatter
     formatting.stylua, -- lua formatter
-    formatting.gofmt, -- golang formatter
+    formatting.gofumpt, -- golang formatter
     formatting.goimports, -- golang formatter
     diagnostics.eslint_d.with({ -- js/ts linter
       -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
